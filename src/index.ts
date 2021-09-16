9a0d2d741ebd90837f2e7c962611ac86628e5abb
Andrei Reznikov <andrezniksk@yandex.ru>
2021-09-16 21:33:16 +0800

    Added ability to change the range by dragging two sliders

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,6 @@ import "./index.css";
   $.fn.mySlider = function(options: {}): JQuery {
 
     const $slider = this;
-
-    console.log($slider.width());
     
     class Model {
       config = $.extend({}, options);
@@ -24,25 +22,56 @@ import "./index.css";
           let secondButtonCoords = this.getCoords(this.$secondButton);
           let shiftX1 = ev.pageX - firstButtonCoords.left;
           let shiftX2 = ev.pageX - secondButtonCoords.left;
+          let sliderWidth = $slider.width();
+          let secondButtonWidth = this.$secondButton.width();
           
           $(document).mousemove((ev) => {
-            let left1 = ev.pageX - shiftX2 - sliderCoords.left;
+            let left1 = ev.pageX - sliderCoords.left;
             let right1;
-            let sliderWidth = $slider.width();
-            let secondButtonWidth = this.$secondButton.width();
 
             if (left1 < 0) left1 = 0;
-
             if (sliderWidth && secondButtonWidth) {
               right1 = sliderWidth - secondButtonWidth;
               if (left1 > right1) left1 = right1;
             }
 
             this.$secondButton.css("margin-left", `${left1}px`);
-            this.$rangeBetween.css("width", `${left1}px`);
+            this.$rangeBetween.css("width", `${parseInt(this.$secondButton.css("margin-left")) - parseInt(this.$firstButton.css("margin-left"))}px`);
           });
 
           $(document).mouseup(() => {
+            $(document).off('mousedown');
+            $(document).off('mousemove');
+          });
+        });
+
+        this.$firstButton.mousedown((ev) => {
+          let sliderCoords = this.getCoords($slider);
+          let betweenCoords = this.getCoords(this.$rangeBetween); 
+          let firstButtonCoords = this.getCoords(this.$firstButton);
+          let secondButtonCoords = this.getCoords(this.$secondButton);
+          let shiftX1 = ev.pageX - firstButtonCoords.left;
+          let shiftX2 = ev.pageX - secondButtonCoords.left;
+          let sliderWidth = $slider.width();
+          let firstButtonWidth = this.$firstButton.width();
+          
+          $(document).mousemove((ev) => {
+            let left1 = ev.pageX - sliderCoords.left;
+            let right1;
+
+            if (left1 < 0) left1 = 0;
+            if (sliderWidth && firstButtonWidth) {
+              right1 = sliderWidth - firstButtonWidth;
+              if (left1 > right1) left1 = right1;
+            }
+
+            this.$firstButton.css("margin-left", `${left1}px`);
+            this.$rangeBetween.css("margin-left", `${left1}px`);
+            this.$rangeBetween.css("width", `${parseInt(this.$secondButton.css("margin-left")) - parseInt(this.$firstButton.css("margin-left"))}px`);
+          });
+
+          $(document).mouseup(() => {
+            $(document).off('mousedown');
             $(document).off('mousemove');
           });
         });
