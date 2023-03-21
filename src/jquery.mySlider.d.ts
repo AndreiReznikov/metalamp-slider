48bee29bbfa41bb3b2068aee3cbd837e8a895c9b
AndreiReznikov <andrezniksk@yandex.ru>
2023-03-21 15:04:51 +0800

    the UserConfig parameter is made optional in the description of the pooshkaSlider type #18

--- src/jquery.mySlider.d.ts
+++ src/jquery.mySlider.d.ts
@@ -1,5 +1,5 @@
 interface JQuery {
-  pooshkaSlider: (userConfig: UserConfig) => jQuery<HTMLElement>;
+  pooshkaSlider: (userConfig?: UserConfig) => jQuery<HTMLElement>;
   update: (newData: UserConfig) => JQuery<HTMLElement>;
   destroy: () => JQuery<HTMLElement>;
 }
